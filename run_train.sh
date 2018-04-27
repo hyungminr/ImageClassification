6e8496083bdc7eb52cccf9b4fef7cc4a6ecf5850
hmroh <raingold1347@snu.ac.kr>
2018-04-27 23:22:09 +0900

    Update Data Structure train_test

--- run_train.sh
+++ run_train.sh
@@ -1,2 +1,3 @@
 python2 train.py --model resnet50_imagenet --save resnet50.imagenet.h5 --test True --train example_annotation --epochs 10
 # python2 train.py --model pretrained --pretrained resnet50.imagenet.h5 --save resnet50.imagenet.more.h5 --test True --train example_categorical_folders --epochs 10
+# python2 train.py --model resnet50_imagenet --save resnet50.imagenet.h5 --test True --train example_train_test --validation 0.0 --epochs 10
--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 import cv2
 import argparse
 import numpy as np
@@ -31,6 +31,8 @@ def parse_args():
                         default='pretrainedModel.h5')
     parser.add_argument('--epochs', dest='epochs', help='Number of Epochs for Training',
                         default=10, type=int)
+    parser.add_argument('--validation', dest='validation_split', help='Ratio of Validation Data',
+                        default=0.2, type=float)
     parser.add_argument('--save', dest='save', help='Save Model File Name',
                         default='resnet50.imagenet.h5')
     parser.add_argument('--test', dest='test_model', help='Test Model',
@@ -46,11 +48,13 @@ def parse_args():
 
     return args
 
-def load_data():
+def load_data(mode='train'):
     data_path = os.path.join(os.getcwd(),'data',args.train_data)
     text_file = os.path.join(data_path, 'annotation.txt')
     if os.path.exists(text_file):
         return load_data_from_annotation()
+    elif os.path.isdir(os.path.join(data_path, 'train')) and os.path.isdir(os.path.join(data_path, 'test')):
+        return load_data_from_train_test(mode)
     else:
         return load_data_from_categorical_folders()
 
@@ -89,7 +93,7 @@ def load_data_from_annotation():
             'Label': labels}
     
     return pd.DataFrame(data)
-    
+
 def load_data_from_categorical_folders():
 
     args = parse_args()
@@ -163,6 +167,78 @@ def load_data_from_categorical_folders():
             'Label': new_labels}
     return pd.DataFrame(data)
 
+def load_data_from_train_test(mode='train'):
+
+    args = parse_args()
+    img_col = args.img_size
+    img_row = args.img_size
+    
+    img_files = []
+    labels = []
+    
+    data_path = os.path.join(os.getcwd(), 'data', args.train_data, mode)
+    category_idx = 0
+
+    print 'Loading files from data folder'
+    for category in tqdm(os.listdir(data_path)): # categorical subfolders in data folder
+        img_path = os.path.join(data_path, category)
+        img_count = 0
+        for img_file in os.listdir(img_path):
+            is_image = (img_file.endswith('png') or
+                        img_file.endswith('PNG') or
+                        img_file.endswith('jpg') or
+                        img_file.endswith('JPG') or
+                        img_file.endswith('jpeg') or
+                        img_file.endswith('JPEG') or
+                        img_file.endswith('gif') or
+                        img_file.endswith('GIF') or
+                        img_file.endswith('tif') or
+                        img_file.endswith('TIF'))
+                        
+            if is_image:
+                img_count += 1
+                img_files.append(os.path.join(img_path, img_file))
+                labels.append(category)
+
+        subcategory = [os.path.join(img_path, subfolder) for subfolder in os.listdir(img_path) if os.path.isdir(os.path.join(img_path,subfolder))]
+        for subcat in subcategory:
+            sub_path = os.path.join(img_path, subcat)
+            for img_file in os.listdir(sub_path):
+                is_image = (img_file.endswith('png') or
+                            img_file.endswith('PNG') or
+                            img_file.endswith('jpg') or
+                            img_file.endswith('JPG') or
+                            img_file.endswith('jpeg') or
+                            img_file.endswith('JPEG') or
+                            img_file.endswith('gif') or
+                            img_file.endswith('GIF') or
+                            img_file.endswith('tif') or
+                            img_file.endswith('TIF'))
+                if is_image:
+                    img_count += 1
+                    img_files.append(os.path.join(img_path, img_file))
+                    labels.append(category)
+    imgs = []
+    new_labels = []
+    num_data = len(img_files)
+    idxs = range(num_data)
+    np.random.seed(0)
+    np.random.shuffle(idxs)
+    print 'Image reading from files'
+    for idx in tqdm(idxs):
+        img_path = img_files[idx]
+        label    = labels[idx]
+        img = imread(img_path, mode='RGB')
+        if img.nbytes > 10**3: # Read image file only when its size is larger than 1 kilo bytes
+            img = imresize(img, (img_col, img_row, 3))
+            imgs.append(img)
+            new_labels.append(label)
+        else:
+            continue
+
+    data = {'Image': imgs,
+            'Label': new_labels}
+    return pd.DataFrame(data)
 
 def label_to_categorical(labels, label_dict=None, mode='to_categorical'):
     if mode == 'to_categorical':
@@ -173,7 +249,6 @@ def label_to_categorical(labels, label_dict=None, mode='to_categorical'):
         output = output[0]
     return output, label_dict
 
-
 def construct_data(frame):
     args    = parse_args()
     img_col = args.img_size
@@ -182,8 +257,7 @@ def construct_data(frame):
     outputData, label_dict  = label_to_categorical(frame['Label'])
     input_shape = (img_col, img_row, 3)
     return inputData, outputData, input_shape, label_dict
-        
-    
+
 def get_model(input_shape, class_num):
     args = parse_args()
     
@@ -211,27 +285,38 @@ def get_model(input_shape, class_num):
     
     return model
 
-
 def train_model(model, batch_size=10):
     args = parse_args()
     epochs = args.epochs
+    validation_split = args.validation_split
     weightDir = os.path.join(os.getcwd(), 'weights', args.model)
     if not os.path.exists(weightDir):
         os.makedirs(weightDir)
-    filepath = weightDir + '/weights.epoch_{epoch:02d}.val-loss_{val_loss:.2f}.h5'
-    modelChackpt = keras.callbacks.ModelCheckpoint(filepath,
-                                                   monitor='val_loss',
-                                                   verbose=0,
-                                                   save_best_only=True,
-                                                   save_weights_only=False,
-                                                   mode='auto',
-                                                   period=10)
+    if validation_split > 0:
+        filepath = weightDir + '/weights.epoch_{epoch:02d}.val-loss_{val_loss:.2f}.h5'
+        modelChackpt = keras.callbacks.ModelCheckpoint(filepath,
+                                                       monitor='val_loss',
+                                                       verbose=0,
+                                                       save_best_only=True,
+                                                       save_weights_only=False,
+                                                       mode='auto',
+                                                       period=10)
+    else:
+        filepath = weightDir + '/weights.epoch_{epoch:02d}.loss_{loss:.2f}.h5'
+        modelChackpt = keras.callbacks.ModelCheckpoint(filepath,
+                                                       monitor='loss',
+                                                       verbose=0,
+                                                       save_best_only=True,
+                                                       save_weights_only=False,
+                                                       mode='auto',
+                                                       period=10)
+        
     time_start = datetime.now()
     print '** Training Start, {}'.format(time_start.strftime('%Y-%m-%d %H:%M:%S'))
     history = model.fit(inputData, outputData,
               batch_size=batch_size,
               epochs=epochs,
-              validation_split=0.2,
+              validation_split=validation_split,
               shuffle=True,
               callbacks=[modelChackpt])
     time_end = datetime.now()
@@ -243,27 +328,36 @@ def train_model(model, batch_size=10):
     model.save(modelName)
     print '** Model Saved at [{}] ...'.format(modelName)
 
-    
 def test_model(label_dict, sample_num=4):
     print '**** Start testing the model'
     args = parse_args()
     img_col = args.img_size
     img_row = args.img_size
+    validation_split = 1.0 - args.validation_split
+    
+    if validation_split == 1.0:
+        validation_split = 0.0
     
     modelName = os.path.join(os.getcwd(), 'weights', args.save)
     model = load_model(modelName)
     
-    right = 0
-    f1 = plt.figure(figsize=(20,20))
-    
     num_data = len(frame['Label'])
     
-    idxs = range(int(num_data*0.8), num_data)
+    idxs = range(int(num_data*validation_split), num_data)
     np.random.seed(0)
     np.random.shuffle(idxs)
+    if len(idxs) < sample_num:
+        sample_num = len(idxs)
     idx_count = 0
+    right = 0
+    
+    fig_col  = int(np.ceil(sample_num/2.))
+    fig_row  = 2
+    fig_size = 6
+    f1 = plt.figure(figsize=(fig_row*fig_size, fig_col*fig_size))
+    
     for idx in tqdm(idxs):
-        plt.subplot(int(np.ceil(sample_num/2.)),2,idx_count+1)
+        plt.subplot(fig_col,fig_row,idx_count+1)
         plt.axis('off')
         img = frame['Image'][idx]
         input_img = np.reshape(img, (1, img_col, img_row, 3))
@@ -279,8 +373,8 @@ def test_model(label_dict, sample_num=4):
         idx_count += 1
         if idx_count >= sample_num:
             break
-
     print '** Accuracy : {} ({} / {})'.format(right*100./sample_num, right, idx_count)
+    # plt.show()
     now = datetime.now()
     nowDatetime = now.strftime('%Y_%m_%d-%H%M%S')
     imgResultFileName = os.path.join(os.getcwd(), 'result', nowDatetime + '_' + args.save + '.png')
@@ -295,4 +389,7 @@ if __name__ == "__main__":
     model = get_model(input_shape=input_shape, class_num=len(outputData[0]))
     train_model(model)
     if args.test_model:
+        # uncomment if your data structure follows "train_test" format
+        # frame = load_data(mode='test')
+        # inputData, outputData, input_shape, label_dict = construct_data(frame)
         test_model(label_dict)
